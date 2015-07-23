29620c9fa15a4c07d4c653b2bd4c862a2ad1cc88
Sam Boysel <sboysel@gmail.com>
2015-07-22 22:12:09 -0400

    updates to histogram.R

--- basic/histograms.R
+++ basic/histograms.R
@@ -9,14 +9,12 @@ df <- data.frame(x = rnorm(1000),
 # Basic
 ggplot(data = df,
        mapping = aes(x = x)) +
-  geom_histogram(data = df,
-                 mapping = aes(y = ..density..),
+  geom_histogram(mapping = aes(y = ..density..),
                  binwidth = 0.2)
 
 # with clearer bins
 ggplot(data = df, mapping = aes(x = x)) +
-  geom_histogram(data = df,
-                 mapping = aes(y = ..density..),
+  geom_histogram(mapping = aes(y = ..density..),
                  binwidth = 0.2,
                  fill = "white",
                  colour = "black")
@@ -24,24 +22,37 @@ ggplot(data = df, mapping = aes(x = x)) +
 # with density curve
 ggplot(data = df,
        mapping = aes(x = x)) +
-  geom_histogram(data = df,
-                 mapping = aes(y = ..density..),
+  geom_histogram( mapping = aes(y = ..density..),
                  binwidth = 0.2) +
   geom_density()
 
 # with facets
 ggplot(data = df,
        mapping = aes(x = x)) +
-  geom_histogram(data = df,
-                 mapping = aes(y = ..density..,
+  geom_histogram(mapping = aes(y = ..density..,
                                fill = z),
                  binwidth = 0.2) +
   facet_grid(~ z)
 
 ggplot(data = df,
        mapping = aes(x = x)) +
-  geom_histogram(data = df,
-                 mapping = aes(y = ..density..,
+  geom_histogram(mapping = aes(y = ..density..,
                                fill = z),
                  binwidth = 0.2) +
   facet_grid(y ~ z)
+
+# fill by y, facet by z
+ggplot(data = df,
+       mapping = aes(x = x)) +
+  geom_histogram(mapping = aes(y = ..density..,
+                               fill = factor(y)),
+                 binwidth = 0.2) +
+  facet_grid(~ z)
+
+# violin plots: few distributions of x by factor z
+ggplot(data = df,
+       mapping = aes(x = z, y = x)) +
+  geom_violin(mapping = aes(fill = z),
+              color = "white",
+              alpha = 0.75) +
+  coord_flip()
